3913bc1881ff06b77e2f9234e26adb31fc41a219
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2022-11-24 01:02:13 +0600

    Linux script added

--- 01_Symbol_Table/run.sh
+++ 01_Symbol_Table/run.sh
@@ -1,1 +1,2 @@
-g++ -std=c++11 1905072_SymbolInfo.cpp 1905072_ScopeTable.cpp 1905072_SymbolTable.cpp 1905072.cpp -o main
+g++ 1905072_SymbolInfo.cpp 1905072_ScopeTable.cpp 1905072_SymbolTable.cpp 1905072_main.cpp -o main
+./main && rm main

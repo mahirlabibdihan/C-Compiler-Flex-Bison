03fab7b4a8713a5ad2284611d68e03ce31b78fd4
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2023-01-19 15:52:20 +0600

    Windows script added

--- 04_Semantic_Analysis/run.bat
+++ 04_Semantic_Analysis/run.bat
@@ -1,5 +1,19 @@
+bison -d -y src/parser.y
+mv y.tab.c src/parser.c
+mv y.tab.h include/
+echo 'Generated the parser C file as well the header file'
+g++ -w -c -o build/parser.o src/parser.c
+echo 'Generated the parser object file'
 flex src/scanner.l
-mv lex.yy.c build/
-g++ -std=gnu++11 -w -c -o build/scanner.o build/lex.yy.c
-g++ -std=c++17 src/*.cpp build/scanner.o -o bin/scanner
-"bin/scanner.exe" io/input.txt
+mv lex.yy.c src/scanner.c
+echo 'Generated the scanner C file'
+g++ -w -c -o build/scanner.o src/scanner.c
+echo 'Generated the scanner object file'
+g++ src/*.cpp -c
+mv *.o build/
+echo 'Generated the object files'
+g++ build/*.o -o bin/main.exe 
+echo 'All ready, running'
+"bin/main.exe" io/input.txt
+rm build/*
+rm src/parser.c src/scanner.c include/y.tab.h
